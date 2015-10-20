7d6c4befc60ffd22fb484cf84a530b53aa7168e3
kazukin134 <kazukin1345@gmail.com>
2015-10-20 11:29:51 +0900

    playerのjumpPowerをjsonで変更できるようにした

---
+++ Assets/Pandemic/WorkSpace/Kimura/EnemyLikeStatus/EnemyLikeStatus.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class EnemyLikeStatus : MonoBehaviour {
+
+    [SerializeField]
+    int enemyHP;
+    float  attackPower;
+    int attribute;
+
+
+    EnemyParameter enemyParameter;
+
+	void Start ()
+    {
+        enemyParameter = GetComponent<EnemyParameter>();
+        Debug.Log(enemyParameter.GetAttribute(2));
+    }
+	
+	void Update ()
+    {
+        
+	}
+
+
+    void EnemyLikeStatusManeger()
+    {
+        attribute = Random.Range(0,3);
+        enemyParameter.GetAttribute(attribute);
+
+    }
+}
