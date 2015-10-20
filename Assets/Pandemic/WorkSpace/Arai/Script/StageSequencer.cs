7d1050f93f90cf721c3ceb1914c7fac2999b6c2d
ooHIROoo <hiro1215.turquoise@gmail.com>
2015-10-20 14:57:42 +0900

    ステージ管理追加

---
+++ Assets/Pandemic/WorkSpace/Arai/Script/StageSequencer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class StageSequencer : MonoBehaviour
+{
+
+    public enum Type
+    {
+        STAGE01,
+        STAGE02,
+        STAGE03,
+    };
+
+    BackGroundSetting _back_ground_setting = null;
+    StageNameSetting _stage_name_setting = null;
+
+    [SerializeField]
+    Type _current_type = Type.STAGE01;
+    
+
+    void Start()
+    {
+        _back_ground_setting = FindObjectOfType<BackGroundSetting>();
+        _stage_name_setting = FindObjectOfType<StageNameSetting>();
+    }
+
+    public Type GetCurrentStageType()
+    {
+        return _current_type;
+    }
+
+    public void Transition(Type type)
+    {
+        _current_type = type;
+        var stage_num = (uint)type;
+        _back_ground_setting.StageChange(stage_num);
+        _stage_name_setting.StageChange(stage_num);
+    }
+
+
+}
