c17ebc493c4bdb5613c9028e9bd270c81dc82ece
tom10987 <tom10987@gmail.com>
2015-10-20 13:21:48 +0900

    リソース追加、画面遷移を追加

---
+++ Assets/Pandemic/WorkSpace/tom10987/Scripts/TextBlinking.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextBlinking : MonoBehaviour {
+
+  Text _text;
+
+  [SerializeField]
+  float _blinkSpeed = 0.02f;
+  float _blinkAlpha = 1.0f;
+
+
+  void Start() {
+    _text = GetComponent<Text>();
+  }
+
+  void Update() {
+    _text.color = new Color(0f, 0f, 0f, _blinkAlpha);
+    _blinkAlpha -= _blinkSpeed;
+
+    if (_blinkAlpha < 0.0f) {
+      _blinkAlpha = 0.0f;
+      _blinkSpeed = -_blinkSpeed;
+    }
+    else if (_blinkAlpha > 1.0f) {
+      _blinkAlpha = 1.0f;
+      _blinkSpeed = -_blinkSpeed;
+    }
+  }
+}
